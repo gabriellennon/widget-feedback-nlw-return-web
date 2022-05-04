68341cec71468d822e585197fc50e6e6563cf79c
Gabriel Lennon <gabriellennon7@gmail.com>
2022-05-04 09:13:21 -0300

    scrollbar style

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,25 +1,37 @@
+import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
-    feedbackType: FeedbackType
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
 
 }
-export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
         <>
             <header>
+                <button 
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={onFeedbackRestartRequested}
+                >
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
+                </button>
                 <span className="text-xl leading-6 flex items-center gap-2">
                     <img className="w-6 h-6" src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} />
                     {feedbackTypeInfo.title}
                 </span>
                 <CloseButton />
             </header>
-            <div className="flex py-8 gap-2 w-full">
-                
-            </div>
+
+            <form className="my-4 w-full">
+                <textarea 
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                />
+            </form>
         </>
     )
 }
