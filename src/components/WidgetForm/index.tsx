7e49af526f319381d8a5e04a647642e69466d3ad
Gabriel Lennon <gabriellennon7@gmail.com>
2022-05-06 17:04:25 -0300

    name archive

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton";
 
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
